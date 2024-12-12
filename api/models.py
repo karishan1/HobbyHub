71789ec5653e9253a7558c38c7af65e44def0bf8
Karishan <ec22899@qmul.ac.uk>
2024-12-12 12:16:35 +0000

    Create User, Hobby and Friendship models and resgistered into admin

--- api/models.py
+++ api/models.py
@@ -1,7 +1,83 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 
+class User(AbstractUser):
+    # User model inherits from AbstractUser
+    date_of_birth = models.DateField(null=True, blank=True)
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name="user_groups",
+        blank=True,
+        help_text=("The groups this user belongs to."),
+        verbose_name=("groups"),
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name="user_permissions",
+        blank=True,
+        help_text=("Specific permissions for this user."),
+        verbose_name=("user permissions"),
+    )
+
+    def __str__(self):
+        return self.username
+
+
+class Hobby(models.Model):
+    # Uniquely identifies each hobby to avoid duplicate hobbies
+    hobby_name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.hobby_name
+
+
+class UserHobby(models.Model):
+    # Establishes Many to Many relationship between User and Hobby
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="user_hobbies",)
+    hobby = models.ForeignKey(Hobby,on_delete=models.CASCADE,related_name="hobby_user",)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.hobby.hobby_name}"
+
+    class Meta:
+        # Establishes uniqueness between combination of User and Hobby
+        unique_together = ('user', 'hobby')
+
+
+class Friendship(models.Model):
+    # Establishes a friendship between two users (Many to Many)
+    user = models.ForeignKey(User,on_delete = models.CASCADE, related_name='friendship_intiated')
+    friend = models.ForeignKey(User,on_delete = models.CASCADE, related_name='friendship_received')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # Establishes uniqueness between combination of User and Friend
+        unique_together = ('user', 'friend')
+
+    def __str__(self):
+        return f"{self.user} is friends with {self.friend}"
+    
+class FriendRequest(models.Model):
+    # Allows Friend requests to be sent from one user to another
+    from_user = models.ForeignKey(User,on_delete = models.CASCADE, related_name='friend_request_sent')
+    to_user = models.ForeignKey(User,on_delete = models.CASCADE, related_name='friend_request_received')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('from_user', 'to_user')
+
+    def __str__(self):
+        return f"Friend Request from {self.from_user} to {self.to_user}"
+
+
+
+
+
+
+
+
+# idk what this is it was created before 
 
 class PageView(models.Model):
     count = models.IntegerField(default=0)

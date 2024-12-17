b70315d76f0784a0329cf560489e3c3e4a839b6c
vineji <vineji7@outlook.com>
2024-12-17 16:10:11 +0000

    Sorts users from most to least common hobbies

--- frontend/src/pages/OtherPage.vue
+++ frontend/src/pages/OtherPage.vue
@@ -27,16 +27,52 @@
           data() {
               return {
                   title: "Other Page",
-                  user_arr: [] as User[]
+                  user_arr: [] as User[],
+                  local_user: {
+                    id: 100,
+                    username: "Sajith",
+                    hobbies: ["gaming","golf","football","guitar"]
+                  }
               }
           },
           methods:{
             async fetch_users(){
                 const response = await fetch('http://127.0.0.1:8000/user_list/');
                 const data = await response.json();
-                this.user_arr = data;
+                this.user_arr = this.mostCommonHobbies(data, this.local_user);
+            },
+            mostCommonHobbies(user_arr: User[], local_user: User): User[]{
+              function getCommonHobbyCount(user_hobbies : string[], local_user_hobbies : string[]): number{
+                let commonHobbyCount: number = 0;
 
+                for (let x of user_hobbies){
+                  for (let y of local_user_hobbies){
+                    if (x === y){
+                      commonHobbyCount += 1;
+                      break;
+                    }
+                  }
+                }
+                return commonHobbyCount;
+              }
+
+              for (let i = 0; i < user_arr.length; i++){
+                for (let j = i+1; j < user_arr.length; j++){
+                  const commonHobbyCountA = getCommonHobbyCount(user_arr[i].hobbies,local_user.hobbies);
+                  const commonHobbyCountB = getCommonHobbyCount(user_arr[j].hobbies,local_user.hobbies);
+
+                  if (commonHobbyCountB > commonHobbyCountA){
+                    const cur = user_arr[i];
+                    user_arr[i] = user_arr[j];
+                    user_arr[j] = cur;
+                  }
+                }
+              }
+
+              return user_arr
             }
+
+
           },
           created(){
             this.fetch_users();

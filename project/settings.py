7684f7ee4b05f470dda48f91f0125a3764506f9c
vineji <vineji7@outlook.com>
2025-01-14 16:05:00 +0000

    settings change

--- project/settings.py
+++ project/settings.py
@@ -67,6 +67,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "https://django-psql-persistent-web-apps-ec22977.apps.a.comp-teach.qmul.ac.uk",
 ]
 
 CORS_ALLOW_CREDENTIALS = True

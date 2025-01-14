1531b49046d1fa4bdb7ba02c05f1d701e75e818d
vineji <vineji7@outlook.com>
2025-01-14 16:15:50 +0000

    csrf host change

--- project/settings.py
+++ project/settings.py
@@ -84,6 +84,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "https://group4-web-apps-ec22977.apps.a.comp-teach.qmul.ac.uk",
 ]
 
 
